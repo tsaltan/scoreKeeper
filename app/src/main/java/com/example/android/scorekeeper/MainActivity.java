1580bd52551a148a046e63dc505747834ab72b06
saltant <tilbe.saltan@monitise.com>
2017-11-14 15:54:36 +0300

    just improve original xml to work landscape

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,55 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final LinearLayout linearLayoutTeamAContainer = findViewById(R.id.linear_layout_team_a_container);
-        final LinearLayout linearLayoutTeamBContainer = findViewById(R.id.linear_layout_team_b_container);
-
-        final TextView textViewTeamNameA = linearLayoutTeamAContainer.findViewById(R.id.text_view_team_name);
-        textViewTeamNameA.setText(R.string.team_a);
-        final TextView textViewTeamNameB = linearLayoutTeamBContainer.findViewById(R.id.text_view_team_name);
-        textViewTeamNameB.setText(R.string.team_b);
-
-        scoreTextA = linearLayoutTeamAContainer.findViewById(R.id.scoreText);
-        scoreTextA.setText(R.string.default_score_and_fault_value);
-        scoreTextB = linearLayoutTeamBContainer.findViewById(R.id.scoreText);
-        scoreTextB.setText(R.string.default_score_and_fault_value);
-        faultTextA = linearLayoutTeamAContainer.findViewById(R.id.faultsText);
-        faultTextA.setText(R.string.default_score_and_fault_value);
-        faultTextB = linearLayoutTeamBContainer.findViewById(R.id.faultsText);
-        faultTextB.setText(R.string.default_score_and_fault_value);
-
-        final Button buttonFaultA = linearLayoutTeamAContainer.findViewById(R.id.button_fault);
-        final Button buttonFaultB = linearLayoutTeamBContainer.findViewById(R.id.button_fault);
-        final Button buttonScoreA = linearLayoutTeamAContainer.findViewById(R.id.button_score);
-        final Button buttonScoreB = linearLayoutTeamBContainer.findViewById(R.id.button_score);
-
-        buttonFaultA.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                faultA(v);
-            }
-        });
-
-        buttonFaultB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                faultB(v);
-            }
-        });
-
-        buttonScoreA.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                scoreA(v);
-            }
-        });
-
-        buttonScoreB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                scoreB(v);
-            }
-        });
+        scoreTextA = findViewById(R.id.scoreTextA);
+        scoreTextB = findViewById(R.id.scoreTextB);
+        faultTextA = findViewById(R.id.faultsTextA);
+        faultTextB = findViewById(R.id.faultsTextB);
     }
 
     public void maketoast(String message) {
